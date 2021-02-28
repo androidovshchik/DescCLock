be3e0d237b1841a4057a1da587dcc3399d295c91
vlad <vladkalyuzhnyu@gmail.com>
2021-02-28 13:49:11 +0300

    AlarmService

--- app/src/main/java/defpackage/deskclock/AlarmService.kt
+++ app/src/main/java/defpackage/deskclock/AlarmService.kt
@@ -12,42 +12,41 @@ import com.android.deskclock.provider.Alarm
 import com.android.deskclock.provider.AlarmInstance
 import org.jetbrains.anko.doAsync
 import org.threeten.bp.LocalTime
-import timber.log.Timber
 import java.util.*
+import java.util.concurrent.Future
 
 class AlarmService : JobService() {
 
-    override fun onStartJob(params: JobParameters): Boolean {
-        try {
-            val preferences = Preferences(applicationContext)
-            val time = LocalTime.now().plusHours(preferences.alarmTime)
-            val alarm = Alarm(time.hour, time.minute).apply {
-                label = "Автоматический будильник"
-                daysOfWeek = Weekdays.ALL
-                enabled = true
-                deleteAfterUse = true
-            }
-            val id = preferences.alarmId
-            doAsync {
-                // deleting previous alarm
-                AlarmStateManager.deleteAllInstances(applicationContext, id)
-                Alarm.deleteAlarm(contentResolver, id)
-                // creating next alarm
-                Events.sendAlarmEvent(R.string.action_create, R.string.label_deskclock)
-                val newAlarm = Alarm.addAlarm(contentResolver, alarm)
-                var newInstance = newAlarm.createInstanceAfter(Calendar.getInstance())
-                newInstance = AlarmInstance.addInstance(contentResolver, newInstance)
-                AlarmStateManager.registerInstance(applicationContext, newInstance, true)
-            }
-        } catch (e: Throwable) {
-            Timber.e(e)
-        } finally {
+    private var future: Future<Unit>? = null
 
+    override fun onStartJob(params: JobParameters): Boolean {
+        val preferences = Preferences(applicationContext)
+        val time = LocalTime.now().plusHours(preferences.alarmTime)
+        val alarm = Alarm(time.hour, time.minute).apply {
+            label = "Автоматический будильник"
+            daysOfWeek = Weekdays.ALL
+            enabled = true
+            deleteAfterUse = true
         }
-        return false
+        val id = preferences.alarmId
+        future = doAsync {
+            // deleting previous alarm
+            AlarmStateManager.deleteAllInstances(applicationContext, id)
+            Alarm.deleteAlarm(contentResolver, id)
+            // creating next alarm
+            Events.sendAlarmEvent(R.string.action_create, R.string.label_deskclock)
+            val newAlarm = Alarm.addAlarm(contentResolver, alarm)
+            preferences.alarmId = alarm.id
+            var newInstance = newAlarm.createInstanceAfter(Calendar.getInstance())
+            newInstance = AlarmInstance.addInstance(contentResolver, newInstance)
+            AlarmStateManager.registerInstance(applicationContext, newInstance, true)
+            jobFinished(params, false)
+        }
+        return true
     }
 
     override fun onStopJob(params: JobParameters): Boolean {
+        future?.cancel(true)
         return false
     }
 
